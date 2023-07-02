996de09be4723a7952df7b324e459a645d073db7
unknown <mayagarsar@gmail.com>
2023-07-02 18:49:39 +0530

    first commit for git demo

---
+++ maya.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"MAYA GARSAR"<<endl;
+	
+	return 0;
+	
+}
---
+++ mg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"MAYA GARSAR"<<endl;
+	cout<<"226270307028"<<endll
+	
+	return 0;
+	
+}
